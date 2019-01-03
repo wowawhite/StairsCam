97879eb90af165d180832e12da32b36491d68ea5
wowawhite <trash_bin@gmx.de>
2019-01-03 11:01:31 +0100

    updated. Check todo

--- CamSocketTest.py
+++ CamSocketTest.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 # -*- coding: <UTF-8> -*-
 # This is client.py file# import socket
+#TODO serialize binary to float data
 #TODO plot data
 #TODO read camera every sec
-#TODO write module
+#TODO write as class
 #
 
 import numpy as np
 import socket
 
-def Main():
+def read_camera():
 	#check out IP of camera
 	cameraIP = "192.168.178.69"
 	controlport = 8080
@@ -20,8 +21,9 @@ def Main():
 	data_length = 12800
 
 	# protocol commands
-	letter_D = 'D'.encode('ascii')
-	letter_q = 'q'.encode('ascii')
+	letter_D = 'D'.encode('ascii')  # radial distance
+	letter_Z = 'Z'.encode('ascii')  # cartesian z distance
+	letter_q = 'q'.encode('ascii')  # stop socket connection
 	# numpy parameter
 	dt = np.dtype('b')  # byte, native byte order
 	#data_bytes = np.array(package_length, dt)
@@ -42,8 +44,8 @@ def Main():
 
 	# connection timeout how much needed?
 
-	print("Sending data request D")
-	clientSocket.sendall(letter_D)
+	print("Sending data request")
+	clientSocket.sendall(letter_Z)
 
 	print("Waiting for response")
 	try:
@@ -52,18 +54,20 @@ def Main():
 		# The return value is a string representing the data received.
 	except socket.timeout:
 		print("Timeout Exception")
-	header_long = np.fromstring(header_bin)
-	data_long = np.fromstring(data_bin)
-	data_long.shape = (50,64) #row,column
+
+	clientSocket.sendall(letter_q)
+	#clientSocket.shutdown()
+	clientSocket.close()
+
+	header_long = np.fromstring(header_bin, 'b')
+	data_long = np.fromstring(data_bin, 'b')
+	print("datatyp: ", type(data_long))
+	#data_long.shape = (50, 64) #row,column
 
 	print("Data received/n")
 	#while i <= len(data):
 
-	clientSocket.sendall(letter_q)
-
-	#clientSocket.shutdown()
-	clientSocket.close()
-
+	#return data_long
 
 	for x in np.nditer(header_long):
 		print(x)
@@ -77,4 +81,4 @@ def Main():
 	f.close()
 	"""
 if __name__ == '__main__':
-	Main()
+	read_camera()
