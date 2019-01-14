e0ec937f3ba8edb63110a605c3acafb9c6dec766
Wowa <no>
2019-01-14 16:41:16 +0100

    cam tcp connection working. todo: parse bytes to float data

--- CamSocketTest.py
+++ CamSocketTest.py
@@ -1,40 +1,28 @@
 #!/usr/bin/python3
 # -*- coding: <UTF-8> -*-
 # This is client.py file# import socket
-#TODO serialize binary to float data
-#TODO plot data
-#TODO read camera every sec
-#TODO write as class
-#
+#TODO byte stream to 4 bytes
+#TODO implement fromstring tostring methods, not frombuffer
 
 import numpy as np
 import socket
+import binascii
 
-import time
-
-
-
-
-
-
-
-def read_camera():
+def Main():
 	#check out IP of camera
-	cameraIP = "192.168.178.69"
+	cameraIP = "192.0.0.69"
 	controlport = 8080
 	videoport = 50002
 	# protocol parameter
-	package_length = 13176  # in bytes
-	# beware data big-endian
+	package_length = 13176 # in bytes
 	header_length = 376
 	data_length = 12800
+	RECV_BUF_SIZE = 13176
+	server_address = (cameraIP, videoport)
 	# protocol commands
-	letter_D = 'D'.encode('ascii')  # radial distance
-	letter_X = 'X'.encode('ascii')  # X cartesian coordinates
-	letter_Y = 'Y'.encode('ascii')  # Y cartesian coordinates
+	letter_D = binascii.a2b_qp('D')
 	letter_Z = 'Z'.encode('ascii')  # cartesian z distance
-	letter_q = 'q'.encode('ascii')  # stop socket connection
-
+	letter_q = 'q'.encode('ascii') # stop socket connection
 	# numpy parameter
 	dt = np.dtype('b')  # byte, native byte order
 	#data_bytes = np.array(package_length, dt)
@@ -47,48 +35,53 @@ def read_camera():
 	dt = np.dtype('d')  # double-precision floating-point number
 	"""
 
+	# socket configuration
 	clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	#clientSocket.setblocking(1)
-	# put exception handling for no connection here
-	clientSocket.settimeout(1.2)
-	clientSocket.connect((cameraIP, videoport))
+	clientSocket.setblocking(True)
+	clientSocket.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
+	clientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, RECV_BUF_SIZE)  # chang rcv buf size
+	clientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # good idea?
+	clientSocket.settimeout(0.5)
 
-	# connection timeout how much needed?
 
-	print("Sending data request")
-	clientSocket.sendall(letter_X)
+	try:
+		clientSocket.connect(server_address)
+	except socket.error as sockerr:
+		print("Socket error: %s" % str(sockerr))
+	except Exception as e:
+		print("Other exception: %s" % str(e))
 
+
+	print("Sending data request D")
+	clientSocket.sendall(letter_D)
+
+
+	# receiving process is bullshit now. loop maybe?
 	print("Waiting for response")
 	try:
+		amount_received = 0
+		while amount_received < 13176:
+			msg_bin = clientSocket.recv(13176)
+			amount_received += len(msg_bin)
 
-		msg_bin = clientSocket.recv(package_length)
-		#msg = msg_bin.decode(encoding='ascii')
-		# wholedata = np.fromstring(, 'b')
-		# The return value is a string representing the data received.
+		print("Data received. %s bytes" % amount_received )
 	except socket.timeout:
 		print("Timeout Exception")
+	except socket.error as sockerr:
+		print("Socket error: %s" % str(sockerr))
 
-	clientSocket.sendall(letter_q)
-	#clientSocket.shutdown()
-	clientSocket.close()
+	if socket:
+		print("Closing connection")
+		clientSocket.sendall(letter_q)
+		clientSocket.close()
 
-	return msg_bin
-"""
-	print("Data received/n")
-	print(len(msg_bin))
-	print(type(msg_bin))
-"""
-for i in range(3):
-	#print(len(read_camera()))
 
-	with open("check{0}.txt".format(i), 'wb') as f:
-		#msg_bin = read_camera()
-		f.write(read_camera())
+	"""
+	dummyWrite = (byte"nope")
+	with open("C:/Users/Wowa/Documents/MEGA/Software/Pycharm/PythonLearning/check.txt", 'wb') as f:
+		f.write(dummyWrite)
 		print("Wrote data to file")
-		f.close()
-		time.sleep(1)
-
-
-
+	f.close()
+	"""
 if __name__ == '__main__':
-	read_camera()
+	Main()
